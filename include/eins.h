f6dd900c1abede81f401a0d6d112b374b0902b28
Alejandro Cosimo <alecosimo@gmail.com>
2015-10-18 23:59:06 +0200

    Added separated header file for utils. Added new util: ISSubsetNumbering()

--- include/eins.h
+++ include/eins.h
@@ -3,6 +3,7 @@
 
 #include <petsc.h>
 #include <petsc/private/petscimpl.h>
+#include "einsutils.h" 
 
 
 /* ---------------------------------------------------------------- */
@@ -12,10 +13,6 @@ PETSC_EXTERN PetscErrorCode EinsInitialize(int*,char***,const char[],const char[
 PETSC_EXTERN PetscErrorCode EinsInitialized(PetscBool *);
 PETSC_EXTERN PetscErrorCode EinsFinalize(void);
 PETSC_EXTERN PetscErrorCode EinsFinalized(PetscBool *);
-
-/* ---------------------------------------------------------------- */
-/*   Some utils */
-PETSC_EXTERN PetscErrorCode VecSeqViewSynchronized(Vec);
 
 
 #define KSPPJGMRES "pjgmres"
---
+++ include/einsutils.h
@@ -0,0 +1,4 @@
+/* ---------------------------------------------------------------- */
+/*   Some utils */
+PETSC_EXTERN PetscErrorCode VecSeqViewSynchronized(Vec);
+PETSC_EXTERN PetscErrorCode ISSubsetNumbering(IS,IS,PetscInt*,IS*);
