721b86cef1d33bfa4470549d28c2f3693ea645ae
Alejandro Cosimo <alecosimo@gmail.com>
2016-05-12 22:23:23 +0200

    cmake configuration file updated

--- cmake/cmakeSys/FindSLEPc.cmake
+++ cmake/cmakeSys/FindSLEPc.cmake
@@ -5,12 +5,18 @@
 #  SLEPC_LIBRARIES    - Link these to use SLEPC
 
 
-find_path(SLEPC_DIR include/slec.h HINTS ENV SLEPC_DIR DOC "SLEPC top-level directory")
-set(SLEPC_ARCH $ENV{PETSC_ARCH} CACHE STRING "SLEPC build")
-find_path(SLEPC_INCLUDE_DIR slepc.h HINTS "${SLEPC_DIR}" PATH_SUFFIXES include NO_DEFAULT_PATH DOC "SLEPC include path")
-mark_as_advanced(SLEPC_INCLUDE_DIR)
-set(SLEPC_INCLUDES ${SLEPC_INCLUDE_DIR} CACHE PATH "SLEPC include paths" FORCE)
-find_library(SLEPC_LIBRARIES NAMES slepc HINTS "${SLEPC_DIR}" PATH_SUFFIXES "${PETSC_ARCH}/lib" "lib" NO_DEFAULT_PATH)
+find_path(SLEPC_DIR include/slepc.h HINTS ENV SLEPC_DIR DOC "SLEPC top-level directory")
+
+if (NOT(${SLEPC_DIR} STREQUAL "SLEPC_DIR-NOTFOUND"))
+  set(SLEPC_ARCH $ENV{PETSC_ARCH} CACHE STRING "SLEPC build")
+  find_path (SLEPC_INCLUDE_CONF slepcconf.h HINTS "${SLEPC_DIR}" PATH_SUFFIXES "${PETSC_ARCH}/include" "bmake/${PETSC_ARCH}" NO_DEFAULT_PATH)
+  find_path(SLEPC_INCLUDE_DIR slepc.h HINTS "${SLEPC_DIR}" PATH_SUFFIXES include NO_DEFAULT_PATH DOC "SLEPC include path")
+  mark_as_advanced(SLEPC_INCLUDE_DIR SLEPC_INCLUDE_CONF)
+  set(SLEPC_INCLUDES ${SLEPC_INCLUDE_DIR} ${SLEPC_INCLUDE_CONF} CACHE PATH "SLEPC include paths" FORCE)
+  mark_as_advanced(SLEPC_INCLUDES)
+  find_library(SLEPC_LIBRARIES NAMES slepc HINTS "${SLEPC_DIR}" PATH_SUFFIXES "${PETSC_ARCH}/lib" "lib" NO_DEFAULT_PATH)
+  mark_as_advanced(SLEPC_LIBRARIES)
+endif (NOT(${SLEPC_DIR} STREQUAL "SLEPC_DIR-NOTFOUND"))
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SLEPC
